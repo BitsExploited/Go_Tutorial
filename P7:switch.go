b75a1690eabdaccf87cc3f389b4a645ecbc2c401
Hash-Cracker <tspamiitesh@gmail.com>
2025-01-01 20:31:00 +0530

    Adding the switch file

---
+++ P7:switch.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func main(){
+
+  grade := 'A'
+    switch grade {
+    case 'A':
+        fmt.Println("Good Job")
+    case 'B':
+        fmt.Println("Good, Do better next time")
+    case 'C':
+        fmt.Println("C grade, Must improve")
+    }
+}

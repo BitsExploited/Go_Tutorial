2ab6bf0837584edf9b4c6a8d2d3318aad944d463
Hash-Cracker <tspamiitesh@gmail.com>
2025-01-01 18:14:23 +0530

    Adding constants file

---
+++ P4:constants.go
@@ -0,0 +1,21 @@
+package main
+import (
+  "fmt"
+  "math"
+)
+
+const s string = "String" // Similar to that of "var", "const" can also be decalred outside a function
+
+func main(){
+  fmt.Println(s)
+
+  const n = 6000000
+
+  const d = 3e20 / n
+  fmt.Println(d)
+
+  fmt.Println(int64(d))
+
+  fmt.Println(math.Sin(n))
+
+}

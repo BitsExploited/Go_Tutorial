9517166cc0e1618d199e3cb114d8bfd9806e7876
Hash-Cracker <tspamiitesh@gmail.com>
2025-01-04 20:29:43 +0530

    Adding defer file

--- P15:defer.go
+++ P15:defer.go
@@ -7,3 +7,5 @@ func main(){
 
   fmt.Println("Hash ")
 }
+
+// This the defer function is run immediately but the funciton is called only after the nearby functions are called and executes at the last

7a6efb1dff286431001e3ef08e015360a5455357
Hash-Cracker <tspamiitesh@gmail.com>
2025-01-05 17:01:08 +0530

    Adding the struct file

---
+++ P18:struct.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+type two_num struct {
+  A int
+  B int
+}
+
+func main(){
+  fmt.Println(two_num{42, 24}) // The numbers 42 and 24 takes the datatype integer respectively and as A and B
+}

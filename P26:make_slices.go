6f553c8ee0b7b50a04d7db5d8ee74ab71104adb1
Hash-Cracker <tspamiitesh@gmail.com>
2025-01-12 07:19:46 +0530

    Adding the Make slices file

--- P26:make_slices.go
+++ P26:make_slices.go
@@ -5,4 +5,9 @@ import "fmt"
 func main() {
   a := make([]int, 5) // The first arg is specifying the datatype of the array and the secidn arg is for the size of the slice or array
   fmt.Printf("The array printed is: %v", a)
+
+  b := make([]int, 0, 5) // The third arg is for mentioning the capacity of the array
+  fmt.Printf("The length of the array is: %d\n", len(b))
+  fmt.Printf("The capacity of the array is: %d\n", cap(b))
+  fmt.Printf("The resulting array is: %v\n", b)
 }
